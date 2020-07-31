54d86ee4a79f53772dd52481baa025beb9d3934f
3dp-mark <round.markl@gmail.com>
2020-07-31 17:02:53 +0100

    Update Configuration.h
    Changed #define CUSTOM_MACHINE_NAME "Ender-3 Marlin v.2.0.6.ABL-BL" // changed 31-07-2020 - 3dpmarkr

--- My Marlin 2.0.6 config files/Marlin/Configuration.h
+++ My Marlin 2.0.6 config files/Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Darkpaw - v.2.0.6.ABL-BL" // changed 31-07-2020 - 3dpmarkr
+#define CUSTOM_MACHINE_NAME "Ender-3 Marlin v.2.0.6.ABL-BL" // changed 31-07-2020 - 3dpmarkr
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
