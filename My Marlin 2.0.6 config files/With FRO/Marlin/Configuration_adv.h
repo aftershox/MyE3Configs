7f4f8d80cfa9a8cb61ff0c735fc3cc597e027a95
3dp-mark <round.markl@gmail.com>
2020-08-02 15:28:05 +0100

    Update Configuration_adv.h
    Increased value of FILAMENT_CHANGE_UNLOAD_LENGTH to 480 - to allow for all filament to be unloaded prior to Filament Change

--- My Marlin 2.0.6 config files/With FRO/Marlin/Configuration_adv.h
+++ My Marlin 2.0.6 config files/With FRO/Marlin/Configuration_adv.h
@@ -2047,7 +2047,7 @@
                                                   // This short retract is done immediately, before parking the nozzle.
   #define FILAMENT_CHANGE_UNLOAD_FEEDRATE     10  // (mm/s) Unload filament feedrate. This can be pretty fast.
   #define FILAMENT_CHANGE_UNLOAD_ACCEL        25  // (mm/s^2) Lower acceleration may allow a faster feedrate.
-  #define FILAMENT_CHANGE_UNLOAD_LENGTH      400  // (mm) The length of filament for a complete unload.
+  #define FILAMENT_CHANGE_UNLOAD_LENGTH      480  // changed 02-08-2020 - 3dpmarkr // DEFAULT 400 (mm) The length of filament for a complete unload.
                                                   //   For Bowden, the full length of the tube and nozzle.
                                                   //   For direct drive, the full length of the nozzle.
                                                   //   Set to 0 for manual unloading.
